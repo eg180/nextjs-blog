77be00460ec98ce5fb9bbc7421a1a353cc14bf00
Erick Gonzalez <ericksinbox@gmail.com>
2021-10-01 15:12:07 +0200

    initial commit

---
+++ pages/api/hello.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+	res.status(200).json({ text: "Hello" });
+}
+
+// tips about APIs in Next.js
+// https://nextjs.org/learn/basics/api-routes/api-routes-details
+
+// even more
+// https://nextjs.org/docs/api-routes/introduction
---
+++ pages/posts/[id].js
@@ -0,0 +1,44 @@
+import utilStyles from "../../styles/utils.module.css";
+import Layout from "../../components/layout";
+import Head from "next/head";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+
+// for this to work, this page file must contain:
+// 1. A React component to render this page
+// 2. getStaticPaths, which returns an array of possible values for **id**
+// 3. getStaticProps, which fetches necessary data for the post with **id**
+
+export async function getStaticProps({ params }) {
+	const postData = await getPostData(params.id);
+	return {
+		props: {
+			postData,
+		},
+	};
+}
+
+export async function getStaticPaths() {
+	const paths = getAllPostIds();
+	return {
+		paths,
+		fallback: false,
+	};
+}
+
+export default function Post({ postData }) {
+	return (
+		<Layout>
+			<Head>
+				<title>{postData.title}</title>
+			</Head>
+			<article>
+				<h1 className={utilStyles.headingXl}>{postData.title}</h1>
+				<div className={utilStyles.lightText}>
+					<Date dateString={postData.date} />
+				</div>
+				<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+			</article>
+		</Layout>
+	);
+}
